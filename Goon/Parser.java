4f41ad503178e8615d78bfed41caeecd522925a2
Tony <tony.yang98@gmail.com>
2020-07-20 20:13:03 -0400

    implement grammar without error handling

---
+++ Goon/Parser.java
@@ -0,0 +1,121 @@
+package Goon;
+
+import java.util.List;
+import static Goon.TokenType.*;
+
+class Parser {
+  private final List<Token> tokens;
+  private int current = 0;
+
+  Parser(List<Token> tokens) {
+    this.tokens = tokens;
+  }
+
+  private Expr expression() {
+    return equality();
+  }
+
+  private Expr equality() {
+    Expr expr = comparison();
+
+    while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+      Token operator = previous();
+      // parse right-hand operand
+      Expr right = comparison();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+    return expr;
+  }
+
+  private Expr comparison() {
+    Expr expr = addition();
+
+    while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+      Token operator = previous();
+      Expr right = comparison();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+    return expr;
+  }
+
+  private Expr addition() {
+    Expr expr = multiplication();
+
+    while (match(MINUS, PLUS)) {
+      Token operator = previous();
+      Expr right = comparison();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+    return expr;
+  }
+
+  private Expr multiplication() {
+    Expr expr = unary();
+
+    while (match(SLASH, STAR)) {
+      Token operator = previous();
+      Expr right = comparison();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+    return expr;
+  }
+
+  private Expr unary() {
+    if (match(BANG, MINUS)) {
+      Token operator = previous();
+      Expr right = unary();
+      return new Expr.Unary(operator, right);
+    }
+    return primary();
+  }
+
+  private Expr primary() {
+    if (match(FALSE))
+      return new Expr.Literal(false);
+    if (match(TRUE))
+      return new Expr.Literal(true);
+    if (match(NIL))
+      return new Expr.Literal(null);
+
+    if (match(NUMBER, STRING)) {
+      return new Expr.Literal(previous().literal);
+    }
+  }
+
+  private boolean match(TokenType... types) {
+    for (TokenType type : types) {
+      if (check(type)) {
+        advance();
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean check(TokenType type) {
+    if (isAtEnd())
+      return false;
+    return peek().type == type;
+  }
+
+  private Token advance() {
+    if (!isAtEnd())
+      current++;
+    return previous();
+  }
+
+  // checks if tokens have run out to parse
+  private boolean isAtEnd() {
+    return peek().type == EOF;
+  }
+
+  // returns current token we have yet to consume
+  private Token peek() {
+    return tokens.get(current);
+  }
+
+  // returns the most recentl consumed token
+  private Token previous() {
+    return tokens.get(current - 1);
+  }
+}
